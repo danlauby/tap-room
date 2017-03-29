5ebf3980cf9d67780528da6394d5bb4be6addfe1
Dan Lauby and Jay Freeman <jay@>
2017-03-29 11:42:46 -0700

    percent char. to percents on html and begin to define a strong beer method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Get Sum!';
   beers: Beer[] = [
-    new Beer('Lonely', 'St. Pauli Girl', 10, '7.9%',124),
-    new Beer('Grumpy', 'Corona', 4, '5.5%',110),
-    new Beer('Sleepy', 'Bud', 3, '4.9%',58),
-    new Beer('Fluffy', 'Stella Artois', 8, '13.9',9),
-    new Beer('Dopey', 'Fat Tire', 2, '2.9%',3),
+    new Beer('Lonely', 'St. Pauli Girl', 10, 4.9, 124),
+    new Beer('Grumpy', 'Corona', 4, 7.2, 110),
+    new Beer('Sleepy', 'Bud', 3, 5.5, 58),
+    new Beer('Fluffy', 'Stella Artois', 8, 13.9, 9),
+    new Beer('Dopey', 'Fat Tire', 2, 4.0, 3),
   ];
+
   beerSpecial(beer) {
     if (beer.price > 4) {
       return "bg-info";
@@ -21,9 +22,16 @@ export class AppComponent {
       return "bg-success";
     }
   }
+
+  beerStrong(beer) {
+    if (beer.percent > 6) {
+      return beer.percent +
+    }
+  }
+
 }
 
 export class Beer {
   public done: boolean = false;
-  constructor(public name: string, public brand: string, public price: number, public percent: string, public amount: number) { }
+  constructor(public name: string, public brand: string, public price: number, public percent: number, public amount: number) { }
 }
