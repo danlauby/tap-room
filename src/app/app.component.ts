b7599a3fb88fe2ad5d7c7105da7fea0e94b1656b
Dan Lauby and Jay Freeman <jay@>
2017-03-29 11:14:32 -0700

    add an unordered list of 5 beers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Get Sum!';
+  beers: Beer[] = [
+    new Beer('Lonely', 'St. Pauli Girl', 5, '7.9%'),
+    new Beer('Grumpy', 'Corona', 4, '5.5%'),
+    new Beer('Sleepy', 'Bud', 3, '4.9%'),
+    new Beer('Fluffy', 'Stella Artois', 8, '13.9'),
+    new Beer('Dopey', 'Fat Tire', 2, '2.9%'),
+  ];
+
+}
+
+export class Beer {
+  public done: boolean = false;
+  constructor(public name: string, public brand: string, public price: number, public percent: string) { }
 }
