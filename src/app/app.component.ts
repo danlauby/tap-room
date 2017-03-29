610c8234b46ff3bb10d1da372ceddd09d83db4f9
Dan Lauby and Jay Freeman <jay@>
2017-03-29 11:29:26 -0700

    Create method to highlight beer objects by price

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,16 +8,22 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Get Sum!';
   beers: Beer[] = [
-    new Beer('Lonely', 'St. Pauli Girl', 5, '7.9%'),
-    new Beer('Grumpy', 'Corona', 4, '5.5%'),
-    new Beer('Sleepy', 'Bud', 3, '4.9%'),
-    new Beer('Fluffy', 'Stella Artois', 8, '13.9'),
-    new Beer('Dopey', 'Fat Tire', 2, '2.9%'),
+    new Beer('Lonely', 'St. Pauli Girl', 10, '7.9%',124),
+    new Beer('Grumpy', 'Corona', 4, '5.5%',110),
+    new Beer('Sleepy', 'Bud', 3, '4.9%',58),
+    new Beer('Fluffy', 'Stella Artois', 8, '13.9',9),
+    new Beer('Dopey', 'Fat Tire', 2, '2.9%',3),
   ];
-
+  beerSpecial(beer) {
+    if (beer.price > 4) {
+      return "bg-info";
+    } else {
+      return "bg-success";
+    }
+  }
 }
 
 export class Beer {
   public done: boolean = false;
-  constructor(public name: string, public brand: string, public price: number, public percent: string) { }
+  constructor(public name: string, public brand: string, public price: number, public percent: string, public amount: number) { }
 }
